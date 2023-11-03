828cbcc1dcd0df6ddcf8dd44007aa9a55f77086e
Frank <frank.rittel@web.de>
2023-11-03 11:14:33 +0100

    Initial Commit

---
+++ TestGit/TestGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitApp.swift
+//  TestGit
+//
+//  Created by Frank on 03.11.23.
+//
+
+import SwiftUI
+
+@main
+struct TestGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
